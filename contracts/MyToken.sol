821420396783acface16ebe1cdf9069c3d053a69
kotkapitan <kapitankott@gmail.com>
2022-06-18 13:04:45 +0200

    first commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20, Ownable {
+contract MyToken is ERC20 {
     constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 100000 * 10 ** decimals());
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
     function mint() public payable {
