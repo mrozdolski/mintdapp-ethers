cb4c155eeb5da2b445ba6cf237e9e27dbcf6e82a
kotkapitan <kapitankott@gmail.com>
2022-06-18 13:01:54 +0200

    first commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
     constructor() ERC20("MyToken", "MTK") {
